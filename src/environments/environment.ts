82a46acc5bfbbe560d784725319e2790ac4dd6c0
Dayud Moahamed <voiphandy@gmail.com>
2019-01-23 07:26:41 +0300

    Correcting environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrlUsers: "https://api.github.com/users/",
   tokenUsers: "?f9316b65766fb257d920d126e68754f05eeb85ea"
 };
