5f564aaab39e02430b6ab32804b1ce02258a4065
Dayud Moahamed <voiphandy@gmail.com>
2019-01-24 08:21:46 +0300

    Change environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiUrlUsers: "https://api.github.com/users/",
   tokenUsers: "?f9316b65766fb257d920d126e68754f05eeb85ea",
   apiUrlrepos:"https://api.github.com/users/",
-  tokenRepos:"/repos?access_token=4d2dbf7b8f928da5d3ba907ee30d777e6fec8a87"
+  tokenRepos:"/repos?4d2dbf7b8f928da5d3ba907ee30d777e6fec8a87"
 
 };
 
