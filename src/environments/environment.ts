b4337281082abd2188bdc46d76893f448d0b8da6
Dayud Moahamed <voiphandy@gmail.com>
2019-01-25 08:21:04 +0300

    Deploying app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   apiUrlUsers: "https://api.github.com/users/",
   tokenUsers: "?4030bf86a94fbabfa5d712434c40705416c6b6bd",
-  apiUrlrepos:"https://api.github.com/users/",
-  tokenRepos:"/repos?42643e03942689b837d184624f4f94c8ff241df0",
-  apiUrlRepository:"https://api.github.com/search/repositories?q=",
-  tokenRepository:"&access_token=2a83b75207cff345e96684d7dedfb381002f40b2"
+  apiUrlrepos: "https://api.github.com/users/",
+  tokenRepos: "/repos?42643e03942689b837d184624f4f94c8ff241df0",
+  apiUrlRepository: "https://api.github.com/search/repositories?q=",
+  tokenRepository: "&access_token=2a83b75207cff345e96684d7dedfb381002f40b2"
 };
 
